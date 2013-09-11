9959dd114cf51f0829f13e0346b511422db25220
cn <cn@mycrobase.de>
2013-09-12 01:59:52 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
 description      "Configures tcpproxy from http://spreadspace.org/tcpproxy/"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.1.0"
